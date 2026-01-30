d25d3bcc150ac4308b9febe0bd6660e697619c5e
Asad Khan <asad.khan@init-consulting.de>
2026-01-30 12:02:08 +0100

    updated cors

--- app/core/cors.py
+++ app/core/cors.py
@@ -3,5 +3,6 @@ DEV_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5173",
     "http://[::1]:5173",
     "http://localhost:3000",
-    "https://www.codedbyasad.com"
+    "https://www.codedbyasad.com",
+    "https://*.vercel.app"
 ]
